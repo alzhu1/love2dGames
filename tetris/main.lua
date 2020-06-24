6c0c92477d959447b5a3d8e1bf53a532974b1d03
Alex Zhu <agz003@ucsd.edu>
2020-06-23 18:36:11 -0700

    Change design, use a matrix of blocks to indicate which blocks to draw

--- tetris/main.lua
+++ tetris/main.lua
@@ -2,15 +2,18 @@
 require 'Tetromino'
 
 -- Init dimensions of window
-WINDOW_WIDTH = 1280
+WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 720
 
 -- Font size
 FONT_SIZE = 10
 
 -- Size of squares
-SQUARE_SIZE = 10
+SQUARE_SIZE = 25
 
+-- Number of rows and columns of game
+NUM_ROWS = 20
+NUM_COLS = 10
 
 --[[
     Init function to set variables
@@ -47,18 +50,38 @@ function love.load()
         Z = { 1, 0, 0 } -- red
     }
 
-    -- Keep a bunch of pieces here
-    allPieces = {}
+    --[[
+        TODO: rethink design
 
-    -- Temp pieces
-    for i, pieceType in ipairs(pieceTypes) do
-        allPieces[i] = Tetromino:new(pieceType, {
-            x = math.random() * (WINDOW_WIDTH - 4 * SQUARE_SIZE),
-            y = math.random() * (WINDOW_HEIGHT - 4 * SQUARE_SIZE)
-        })
+        currently thinking of having an "activePiece" and "nextPiece" be Tetrominoes
+        once they land/can't move anymore, swap pieces, and set blocks in a "total" map
+
+        10 x 20 bottom-up map (aka row 1 is the bottom row)
+    ]]
+
+    -- Set local vars to leftmost and topmost coordinates
+    local leftX = WINDOW_WIDTH / 2 - (NUM_COLS / 2 * SQUARE_SIZE)
+    local topY = WINDOW_HEIGHT / 2 - (NUM_ROWS / 2 * SQUARE_SIZE)
+
+    -- Create NUM_ROWS x NUM_COLS block mapping
+    blocks = {}
+    for row=1, NUM_ROWS do
+        blocks[row] = { blockCount = 0 }
+        for col=1, NUM_COLS do
+            blocks[row][col] = {
+                x = leftX + (col - 1) * SQUARE_SIZE,
+                y = topY + (20 - row) * SQUARE_SIZE,
+                filled = false,
+                rgb = {row / NUM_ROWS, col / NUM_COLS, row / NUM_ROWS}
+            }
+        end
     end
 
-    timer = 0
+    -- Temp piece
+    activePiece = Tetromino:new("I", { x = leftX, y = topY })
+    nextPiece = nil -- Randomize
+
+    frameCount = 0
 end
 
 --[[
@@ -68,13 +91,11 @@ end
 ]]
 function love.update(dt)
     if gameState == "play" then
-        if timer >= 3 then
-            for _, piece in ipairs(allPieces) do
-                piece:rotate(false)
-            end
-            timer = 0
+        if frameCount == 48 then
+            activePiece:move()
+            frameCount = 0
         else
-            timer = timer + dt
+            frameCount = frameCount + 1
         end
     end
 end
@@ -84,10 +105,21 @@ end
 ]]
 function love.draw()
     -- Draw each piece
-    for _, piece in ipairs(allPieces) do
-        local pieceType = piece.pieceType
-        piece:draw(pieceTypeToColor[pieceType])
+    -- for _, piece in ipairs(allPieces) do
+    --     local pieceType = piece.pieceType
+    --     piece:draw(pieceTypeToColor[pieceType])
+    -- end
+
+    for _, row in ipairs(blocks) do
+        for a, col in ipairs(row) do
+            love.graphics.setColor(col.rgb)
+            love.graphics.rectangle("fill", col.x, col.y, SQUARE_SIZE, SQUARE_SIZE)
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.rectangle("line", col.x, col.y, SQUARE_SIZE, SQUARE_SIZE)
+        end
     end
+
+    activePiece:draw(pieceTypeToColor[activePiece.pieceType])
 end
 
 -- TODO: keep this for tetris collisions?
